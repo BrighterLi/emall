0c4ea42d92e34510a0f0bb9b8d5acd9fc6a152c1
liming <287055586@qq.com>
2019-09-15 00:29:23 +0800

    http请求返回数据并显示

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,25 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController  typeController = TextEditingController();
+  String showText = '欢迎您来到美好人间高级会所';
+
   @override
   Widget build(BuildContext context) {
-    getHttp();
     return Scaffold(
-      body: Center(child: Text('商城首页'),
+      appBar: AppBar(title: Text('美好人间'),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: typeController,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(10.0),
+                labelText: '美女类型',
+                helperText: '请输入你喜欢的类型',
+              ),
+              autofocus: false,
+            ),
+            RaisedButton(
+              onPressed: _choiceAction,
+              child: Text('选择完毕'),
+            ),
+            Text(
+              showText,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+            )
+          ],
+        ),
       ),
     );
   }
 
-  void getHttp() async {
+  void _choiceAction(){
+    print('开始选择你喜欢的类型......');
+    if(typeController.text.toString() == '') {
+      showDialog(
+        context: context,
+        builder: (context)=>AlertDialog(title: Text('美女类型不能为空'),)
+      );
+    }else {
+        getHttp(typeController.text.toString()).then((val) {
+          setState(() {
+            showText = val['data']['name'].toString();
+          });
+        });
+    }
+  }
+  Future getHttp(String typeText) async {
     try {
       Response response;
-      response = await Dio().get(
-        "https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=长腿美女",
-      );
-      return print(response);
+      var data = {'name':typeText};
+      response = await Dio().get("https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian",
+            queryParameters: data
+          );
+      return response.data;
     } catch (e) {
       return print(e);
     }
+
   }
 }
