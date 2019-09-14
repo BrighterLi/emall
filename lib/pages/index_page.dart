665d34e5f23c547de6c4e505665e860aa1a069ef
liming <287055586@qq.com>
2019-09-14 22:42:19 +0800

    底部tab点击跳转效果

--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-
+import 'home_page.dart';
+import 'category_page.dart';
+import 'cart_page.dart';
+import 'member_page.dart';
 
 class IndexPage extends StatefulWidget {
   @override
@@ -26,9 +29,40 @@ class _IndexPageState extends State<IndexPage> {
       title:Text('会员中心'),
     ),
   ];
+
+  final List tabBodies = [
+    HomePage(),
+    CategoryPage(),
+    CartPage(),
+    MemberPage(),
+  ];
+
+  int currentIndex = 0;
+  var currentPage;
+
+  @override
+  void initState() {
+    currentPage = tabBodies[currentIndex];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return  Scaffold(
+      backgroundColor: Color.fromRGBO(244, 2245, 245, 1.0),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        items: bottomTabs,
+        onTap: (index){
+          setState(() {
+            currentIndex = index;
+            currentPage = tabBodies[currentIndex];
+          });
+        }
+      ),
+      body: currentPage,
+    );
   }
 }
 
