460db2e6e3ccf1781a6e0d7f42cf5939afd59f1a
liming <287055586@qq.com>
2019-09-14 22:40:32 +0800

    底部tab效果

---
+++ lib/pages/index_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+
+class IndexPage extends StatefulWidget {
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  final List<BottomNavigationBarItem> bottomTabs = [
+    BottomNavigationBarItem(
+      icon:Icon(CupertinoIcons.home),
+      title:Text('首页'),
+  ),
+    BottomNavigationBarItem(
+      icon:Icon(CupertinoIcons.search),
+      title:Text('分类'),
+    ),
+    BottomNavigationBarItem(
+      icon:Icon(CupertinoIcons.shopping_cart),
+      title:Text('购物车'),
+    ),
+    BottomNavigationBarItem(
+      icon:Icon(CupertinoIcons.profile_circled),
+      title:Text('会员中心'),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
