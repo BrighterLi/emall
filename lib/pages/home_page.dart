9c1f9f031f8f8565088f0429234b4f5eb804b674
liming <287055586@qq.com>
2019-09-16 01:27:32 +0800

    请求首页后台数据

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
+import '../service/service_method.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,73 +7,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController  typeController = TextEditingController();
-  String showText = '欢迎您来到美好人间高级会所';
+  String homePageContent = '正在获取数据';
+
+  @override
+  void initState() {
+    getHomePageContent().then((val) {
+    //getHomePageBeloConten().then((val) {
+      setState(() {
+        homePageContent = val.toString();
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('美好人间'),
+      appBar: AppBar(
+        title: Text('百姓生活+'),
       ),
       body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              TextField(
-                controller: typeController,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(10.0),
-                  labelText: '美女类型',
-                  helperText: '请输入你喜欢的类型',
-                ),
-                autofocus: false,
-              ),
-              RaisedButton(
-                onPressed: _choiceAction,
-                child: Text('选择完毕'),
-              ),
-              Text(
-                showText,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 1,
-              )
-            ],
-          ),
-        ),
+          child: Text(homePageContent)
       ),
     );
-  }
-
-  void _choiceAction(){
-    print('开始选择你喜欢的类型......');
-    if(typeController.text.toString() == '') {
-      showDialog(
-        context: context,
-        builder: (context)=>AlertDialog(title: Text('美女类型不能为空'),)
-      );
-    }else {
-        getHttp(typeController.text.toString()).then((val) {
-          setState(() {
-            showText = val['data']['name'].toString();
-          });
-        });
-    }
-  }
-
-  Future getHttp(String typeText) async {
-    try {
-      Response response;
-      var data = {'name':typeText};
-      //https://easy-mock.com/mock/5d7d28156028ae281ebf0aab/brightli/get_test
-      //https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian
-      response = await Dio().get("https://easy-mock.com/mock/5d7d28156028ae281ebf0aab/brightli/get_test",
-          queryParameters: data
-      );
-      print(response);
-      return response.data;
-    } catch (e) {
-      return print(e);
-    }
-
   }
 }
--- lib/service/service_method.dart
+++ lib/service/service_method.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:dio/dio.dart';
 import 'dart:async';
 import 'dart:io';
@@ -20,4 +21,56 @@ Future getHomePageContent() async {
   } catch (e) {
     return print('ERROR: ===========>${e}');
   }
+}*/
+
+import 'package:dio/dio.dart';
+import 'dart:async';
+import 'dart:io';
+import '../config/service_url.dart';
+
+
+
+Future getHomePageContent() async{
+
+  try{
+    print('开始获取首页数据...............');
+    Response response;
+    Dio dio = new Dio();
+    dio.options.contentType=ContentType.parse("application/x-www-form-urlencoded");
+    var formData = {'lon':'115.02932','lat':'35.76189'};
+    //response = await dio.get("http://www.baidu.com");
+    response = await dio.post(servicePath['homePageContent'],data:formData);
+    print("response.data");
+    print(response.data);
+    if(response.statusCode==200){
+      return response.data;
+    }else{
+      throw Exception('后端接口出现异常，请检测代码和服务器情况.........');
+    }
+  }catch(e){
+    return print('ERROR:======>${e}');
+  }
+
+}
+
+//获得火爆专区商品的方法
+Future getHomePageBeloConten() async{
+
+  try{
+    print('开始获取下拉列表数据.................');
+    Response response;
+    Dio dio = new Dio();
+    dio.options.contentType=ContentType.parse("application/x-www-form-urlencoded");
+    int page=1;
+    response = await dio.post(servicePath['homePageBelowConten'],data:page);
+    if(response.statusCode==200){
+      return response.data;
+    }else{
+      throw Exception('后端接口出现异常，请检测代码和服务器情况.........');
+    }
+  }catch(e){
+    return print('ERROR:======>${e}');
+  }
+
+
 }
