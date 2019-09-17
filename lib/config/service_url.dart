32d39cb795b6a593253a3efcec8c44c21e818d8d
liming <287055586@qq.com>
2019-09-17 23:53:26 +0800

    更新服务器地址

--- lib/config/service_url.dart
+++ lib/config/service_url.dart
@@ -1,5 +1,5 @@
+const serviceUrl = 'http://v.jspang.com:8088/baixing/';
 //const serviceUrl = 'http://test.baixingliangfan.cn/baixing/';
-const serviceUrl = 'http://test.baixingliangfan.cn/baixing/';
 const servicePath = {
   'homePageContent': serviceUrl+'wxmini/homePageContent', //商家首页信息
 
